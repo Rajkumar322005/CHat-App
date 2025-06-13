170b271e427f0ffb88bd95ad10a28d94f4b70abe
Rajkumar322005 <sivasubaramanipkm@gmail.com>
2025-06-13 10:14:18 +0530

    Routings Are Implemented

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Homepage from './pages/Homepage'
+import LoginPage from './pages/LoginPage' 
+import ProfilePage from './pages/ProfilePage'
 
-const App = () => {
+const App
+ = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </div>
   )
 }
 
