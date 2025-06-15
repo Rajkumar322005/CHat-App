4c0cf98f6509d63c1d461b8d76ac7ea258e82b14
Rajkumar322005 <sivasubaramanipkm@gmail.com>
2025-06-15 10:38:42 +0530

    Login Page Initiated

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React,{useState} from 'react'
+import assets from '../assets/assets.js';
+
 
 const LoginPage = () => {
+  const [currstate, setCurrState] = useState('Sign up');
   return (
-    <div>Login Page</div>
+    <div className='min-h-screen bg-cover bg-center flex items-center justify-center gap-8 
+    sm:justify-evenly max-sm:flex-col backdrop-blur-2xl'>
+      {/* {Left} */}
+      <img src={assets.logo_big} alt="Login Illustration" className='w-[min(30vw,250px)]'/>
+
+      {/* {Right} */}
+
+      <form className='border-2 bg-white/8 text-white
+       border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg'>
+        <h2 className='font-medium text-2xl flex justify-between items-center'>
+          {currstate}
+          <img src={assets.arrow_icon} alt="" className='w-5 cursor-pointer'/>
+        </h2>
+
+        {currstate === 'Sign up' && (
+          <>
+            <input type="text" className='p-2 border border-gray-500 rounded-md focus:outline-none' placeholder='Full Name' required/>
+          </>
+        )}  
+
+      </form>
+
+    </div>
   )
 }
 
