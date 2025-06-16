d055efe5c452db462601260cfb28e79d5c3e747b
Rajkumar322005 <sivasubaramanipkm@gmail.com>
2025-06-16 11:52:54 +0530

    Server setup completed

---
+++ server/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import http from "http";
+
+const app = express();
+const server = http.createServer(app);
+app.use(express.json({limit:"4mb"}))
+
+app.use(cors());
+app.use("/api/status", (req,res) => res.send("Sever is running"))
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT,(req,res)=> console.log("Server Is Running on PORT"+PORT));
