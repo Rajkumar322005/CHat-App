2cbf1e4235c35f5426e9cc0d2a3c4af6c862791d
Rajkumar322005 <sivasubaramanipkm@gmail.com>
2025-06-18 11:43:18 +0530

    Auth Created Succesfully

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,10 @@
 //Sign up a new User
 import User from '../models/User.js';
 import { generateToken } from '../lib/util.js';
- 
+import cloudinary from '../lib/cloudinary.js';
+import bcrypt from 'bcryptjs';
+
+
 export const signup = async (req,res) => {
     const { fullName, email , password , bio } = req.body;
 
@@ -35,3 +38,49 @@ export const signup = async (req,res) => {
     }
 }
 
+export const login = async (req,res) => {
+    try{
+        const { email, password } = req.body;
+        const userData = await User.findOne({email});
+        const isPasswordCorrect = await bcrypt.compare(password,userData.password);
+        if(!isPasswordCorrect){
+            return res.json({success:false,message:"Invalid credentials"});
+        }
+
+        const token = generateToken(userData._id);
+        res.json({success:true, userData , token,  message: "Login Successfull"})
+         
+    }
+    catch(error){
+        res.json({success: false,message:error.message});
+        console.log(error.message);
+    }
+}
+
+// Controller to check if user is authenticated
+export const checkAuth = (req,res) =>{
+    res.json({success: true, user: req.user});
+}
+
+// Controller to get user profile by ID
+export const updateProfile = async (req,res) =>{
+    try{
+        const {profilePic,bio,fullName} = req.body;
+
+        const userId = req.user._id;
+        let updatedUser;
+        if(!profilePic) {
+            updatedUser = await User.findByIdAndUpdate(userId, {
+                bio,fullName},{new: true});
+        }
+        else{
+            const upload = await cloudinary.uploader.upload(profilePic);
+            updatedUser = await User.findByIdAndUpdate(userId, {profilePic: upload.secure_url, bio, fullName}, {new: true});
+        }
+        res.json({success: true, user: updatedUser, message: "Profile updated successfully"});
+    }
+    catch(error){
+        res.json({success:false,message:error.message});
+        console.log(error.message); 
+    }
+}
---
+++ server/middleware/auth.js
@@ -0,0 +1,21 @@
+//Middleware to protect routes
+import User from '../models/User.js';
+import jwt, { decode } from 'jsonwebtoken';
+export const productRoute = async (req, res, next) => {
+    try{
+        const token = req.headers.token;
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+
+        const user = await User.findById(decoded.userId).select("-password");
+
+        if(!user){
+            return res.json({success: false, message: "User not found"});
+        }
+        req.user = user;
+        next();
+    }
+    catch(error){
+        res.json({success:false,message:error.message});
+    }
+}
